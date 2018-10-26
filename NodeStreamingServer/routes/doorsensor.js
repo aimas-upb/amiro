72da24e875941f55375e2f156560c07a7198a944
alexandrugavril <alex@wavemotionlabs.com>
2018-10-26 12:47:03 +0300

    Added events endpoint

--- NodeStreamingServer/routes/doorsensor.js
+++ NodeStreamingServer/routes/doorsensor.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+router.post('/events', function(req, res) {
+    console.log(req.body);
+});
+
 router.post('/', function(req, res) {
     console.log(req.body);
-    res.send(req.body);
 });
 
 module.exports = router;
