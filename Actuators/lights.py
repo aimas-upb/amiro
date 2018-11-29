d6e51f8b2366241901e920fd82d07763f86e7905
alexandrugavril <alex@wavemotionlabs.com>
2018-11-29 15:20:04 +0200

    #12

--- Actuators/lights.py
+++ Actuators/lights.py
@@ -22,7 +22,7 @@ class LightsNode:
 rospy.spin()
 
 with open('lights_config.json') as json_data_file:
-	lights_config = json.load(json_data_file)
+    lights_config = json.load(json_data_file)
 
     for key in lights_config:
         bnode = LightsNode(key, light['token'])
