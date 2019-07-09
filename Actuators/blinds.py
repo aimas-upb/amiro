262d5c22d997ba2360bda189bb97ec8533a4e85f
Alex Sorici <alex.sorici@gmail.com>
2019-07-09 19:48:30 +0300

    Updating blinds node with a ROS availability service

--- Actuators/blinds.py
+++ Actuators/blinds.py
@@ -1,10 +1,14 @@
 import rospy
 from std_msgs.msg import Int32
+from amiro_services.srv import IsAvailable, IsAvailableResponse
+
 import RPi.GPIO as GPIO
 import json
 
 
 class BlindsNode:
+	ROS_NODE_NAME = "rpi_1"
+
 	def __init__(self, topic_name, gpio_up, gpio_down):
 		GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD
 		rospy.init_node('rpi_1', anonymous=True)
@@ -14,6 +18,20 @@ class BlindsNode:
 
 		GPIO.setup(gpio_up, GPIO.OUT, initial=1)
 		GPIO.setup(gpio_down, GPIO.OUT, initial=1)
+
+		# setup availability service
+		self.availability_service = None
+		self.__setup_availability_service()
+
+
+	def __setup_availability_service(self):
+		service_name = BlindsNode.ROS_NODE_NAME + "_availability"
+		self.availability_service = rospy.Service(service_name, IsAvailable, self.is_available)
+
+
+	def is_available(self):
+		return IsAvailableResponse(True)
+
 
 	def listen(self):
 		rospy.Subscriber(self.topic_name, Int32, self.on_blinds_msg)
